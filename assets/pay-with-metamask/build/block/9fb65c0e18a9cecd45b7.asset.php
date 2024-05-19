72e3efdea9a113dccbc5373ed419e2234f6d7491
Project Stithulf <72882813+stithulf@users.noreply.github.com>
2024-05-20 03:43:35 +0530

    Add files via upload

---
+++ assets/pay-with-metamask/build/block/9fb65c0e18a9cecd45b7.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '9fb65c0e18a9cecd45b7');
---
+++ assets/pay-with-metamask/build/checkout/30e692326cd10d143d2d.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '30e692326cd10d143d2d');
