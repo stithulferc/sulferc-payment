f1bf909834639ed36ebfe5a0075ed15fcc5ef800
Project Stithulf <72882813+stithulf@users.noreply.github.com>
2024-06-29 14:14:39 +0530

    Delete assets/pay-with-metamask/build/checkout directory

--- assets/pay-with-metamask/build/checkout/30e692326cd10d143d2d.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '30e692326cd10d143d2d');
--- assets/pay-with-metamask/build/checkout/e58ebac913be8a01fa2c.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'e58ebac913be8a01fa2c');
